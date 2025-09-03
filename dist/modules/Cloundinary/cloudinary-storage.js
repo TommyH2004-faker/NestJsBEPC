e3c4a3dc7d7f44f00e1ca6b38e6fed19d89345cb
TommyH <hiept81331@gmail.com>
2025-09-03 08:05:07 +0700

    update pj

--- dist/modules/Cloundinary/cloudinary-storage.js
+++ dist/modules/Cloundinary/cloudinary-storage.js
@@ -5,12 +5,10 @@ const multer_storage_cloudinary_1 = require("multer-storage-cloudinary");
 const cloudinary_config_1 = require("./cloudinary.config");
 exports.storage = new multer_storage_cloudinary_1.CloudinaryStorage({
     cloudinary: cloudinary_config_1.default,
-    params: (req, file) => {
-        return {
-            folder: 'movies',
-            resource_type: 'image',
-            public_id: file.originalname.split('.')[0] + '-' + Date.now(),
-        };
-    },
+    params: (req, file) => ({
+        folder: 'movies',
+        resource_type: 'image',
+        public_id: file.originalname.split('.')[0] + '-' + Date.now(),
+    }),
 });
 //# sourceMappingURL=cloudinary-storage.js.map
