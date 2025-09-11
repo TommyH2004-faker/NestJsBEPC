ce0115531fff47480086ea8b01860d85d72a8a60
TommyH <hiept81331@gmail.com>
2025-09-11 22:10:08 +0700

    update pj lên railway

--- fix-imports.js
+++ fix-imports.js
@@ -1,11 +1,15 @@
+// fix-imports.js
 const fs = require("fs");
 const path = require("path");
 
 function replaceImportsInFile(filePath) {
   let content = fs.readFileSync(filePath, "utf8");
 
-  // Regex: tìm import từ 'src/...'
-  const updated = content.replace(/(['"])src\//g, "$1@src/");
+  // Regex: tìm import có dạng từ "src/..." hoặc '../src/...' hoặc "../../src/..."
+  const updated = content.replace(
+    /(['"])(?:\.\.\/)*src\//g,
+    '$1@src/'
+  );
 
   if (updated !== content) {
     fs.writeFileSync(filePath, updated, "utf8");
